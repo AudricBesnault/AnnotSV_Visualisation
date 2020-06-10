2ae9f9aefd5014c4a17640de17113bff1c9ed90f
BESNAULT-CLERICE AUDRIC <audric.besnault-clerice@etu.unistra.fr>
2020-06-10 05:05:07 +0200

    table is read and db is created

---
+++ sql_request.js
@@ -0,0 +1,85 @@
+'use strict'
+
+const path = require('path')
+const fs = require('fs')
+const SQL = require('sql.js')
+
+let _rowsFromSqlDataObject = function (object) {
+  let data = {}
+  let i = 0
+  let j = 0
+  for (let valueArray of object.values) {
+    data[i] = {}
+    j = 0
+    for (let column of object.columns) {
+      Object.assign(data[i], {[column]: valueArray[j]})
+      j++
+    }
+    i++
+  }
+  return data
+}
+
+SQL.dbOpen = function (databaseFileName) {
+  try {
+    return new SQL.Database(fs.readFileSync(databaseFileName))
+  } catch (error) {
+    console.log("Can't open database file.", error.message)
+    return null
+  }
+}
+
+SQL.dbClose = function (databaseHandle, databaseFileName) {
+  try {
+    let data = databaseHandle.export()
+    let buffer = Buffer.alloc(data.length, data)
+    fs.writeFileSync(databaseFileName, buffer)
+    databaseHandle.close()
+    return true
+  } catch (error) {
+    console.log("Can't close database file.", error)
+    return null
+  }
+}
+
+/*
+  A function to create a new SQLite3 database from table.sql.
+
+  This function is called from main.js during initialization 
+*/
+module.exports.initDb = function (appPath) {
+  let dbPath = path.join(appPath, 'test.db')
+  let createDb = function (dbPath) {
+    // Create a database.
+    let db = new SQL.Database()
+    let query = fs.readFileSync(
+    path.join(__dirname, 'table.sql'), 'utf8')
+    let result = db.exec(query)
+    if (Object.keys(result).length === 0 &&
+      typeof result.constructor === 'function' &&
+      SQL.dbClose(db, dbPath)) {
+      console.log('Created a new database.')
+    } else {
+      console.log('sql_request.initDb.createDb failed.')
+    }
+  }
+  let db = SQL.dbOpen(dbPath)
+  if (db === null) {
+    /* The file doesn't exist so create a new database. */
+    createDb(dbPath)
+  } else {
+    /*
+      The file is a valid sqlite3 database. This simple query will demonstrate
+      whether it's in good health or not.
+    */
+    let query = 'SELECT count(*) as `count` FROM `sqlite_master`'
+    let row = db.exec(query)
+    let tableCount = parseInt(row[0].values)
+    if (tableCount === 0) {
+      console.log('The file is an empty SQLite3 database.')
+      createDb(dbPath)
+    } else {
+      console.log('The database has', tableCount, 'tables.')
+    }
+  }
+}
---
+++ table.sql
@@ -0,0 +1,99 @@
+CREATE TABLE patient (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  sample_id TEXT,
+  sexe CHARACTER(1)
+);
+
+CREATE TABLE sv (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  chrom INTEGER,
+  start INTEGER,
+  end INTEGER,
+  type INTEGER,
+  length INTEGER,
+  chrom_new INTEGER,
+  start_new INTEGER,
+  end_new INTEGER
+);
+
+CREATE TABLE methode (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  descritption TEXT
+);
+
+CREATE TABLE info_annot (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  info TEXT
+);
+
+CREATE TABLE info_sv (
+  info INTEGER,
+  patient_sv INTEGER,
+  PRIMARY KEY (info, patient_sv),
+  FOREIGN KEY (info) REFERENCES info_annot (id),
+  FOREIGN KEY (patient_sv) REFERENCES patient_sv (id)
+);
+
+CREATE TABLE patient_sv (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  patient_id INTEGER,
+  sv_id INTEGER,
+  sv_gene INTEGER,
+  frequence INTEGER,
+  methode INTEGER,
+  id_annot TEXT,
+  ref_annot TEXT,
+  alt_annot TEXT,
+  qual_annot INTEGER,
+  filter TEXT,
+  FOREIGN KEY (patient_id) REFERENCES patient (id),
+  FOREIGN KEY (sv_id) REFERENCES sv (id),
+  FOREIGN KEY (sv_gene) REFERENCES sv_gene (id),
+  FOREIGN KEY (methode) REFERENCES methode (id)
+);
+
+CREATE TABLE patient_sv_commun (
+  patient1 INTEGER,
+  patient2 INTEGER,
+  PRIMARY KEY (patient1, patient2),
+  FOREIGN KEY (patient1) REFERENCES patient_sv (id),
+  FOREIGN KEY (patient2) REFERENCES patient_sv (id)
+);
+
+CREATE TABLE gene (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  hi INTEGER,
+  tris INTEGER,
+  ddd_gene TEXT,
+  acmg BOOLEAN,
+  synz REAL,
+  misz REAL,
+  pli REAL,
+  delz REAL,
+  dupz REAL,
+  cnvz REAL,
+  mim_number INTEGER,
+  phenotype TEXT,
+  inheritance TEXT,
+  morbidGene BOOLEAN,
+  morbidGane_Candidate BOOLEAN
+);
+
+CREATE TABLE sv_gene (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gene INTEGER,
+  sv INTEGER,
+  nm TEXT,
+  cds_length INTEGER,
+  tx_length INTEGER,
+  location TEXT,
+  location2 TEXT,
+  INTEGERersect_start INTEGER,
+  INTEGERersect_end INTEGER,
+  promoter TEXT,
+  FOREIGN KEY (gene) REFERENCES gene (id),
+  FOREIGN KEY (sv) REFERENCES sv (id)
+);
+
